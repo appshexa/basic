6031186e04477ea151fb249f0ac76918f92237c0
Dev01 Hexa <dev01.hexa@gmail.com>
2022-09-29 10:51:33 +0800

    Include TW-Elements

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,12 +10,18 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        @stack('scripts')
+
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+
+        @stack('styles')
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+
+
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
